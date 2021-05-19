39cff4628d095c0831958edba6c9ae8e194ba297
sricharanjitwta <sricharansrichabellur@gmail.com>
2021-05-19 16:15:36 +0530

    Project Updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,4 +1,4 @@
-
+import django_heroku
 import os
 import environ
 
@@ -110,10 +110,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL="todolist"
 LOGIN_URL='login'
+
+django_heroku.settings(locals())
+
